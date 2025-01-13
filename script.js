d8cf3fcd12db3c7a12aac020b9692333dba869af
Utplaksh Mishra <umishra8770@gmail.com>
2025-01-13 07:05:52 +0530

    following new approch making 2 seperate function 1:geting the value 2: setting the value, completed till view

--- script.js
+++ script.js
@@ -82,3 +82,85 @@
 //   // t_views();
 //   // monthsold();
 // };
+console.log("hello");
+
+// =====get thumbnail=======
+function getthumbnail() {
+  let photo = document.getElementById("input_thumbnail").value;
+  localStorage.setItem("photo", photo);
+}
+// ----set thumbnail-----
+const setThumbnail = () => {
+  let photo = localStorage.getItem("photo");
+  let pic = document.getElementById("thumbnail");
+  pic.src = photo;
+};
+// ======get video dureation======
+function dure() {
+  let time = document.getElementById("input_dur").value;
+  localStorage.setItem("time", time);
+}
+// -----Set video dureation------
+function setTime() {
+  let time = localStorage.getItem("time");
+  time = parseInt(time);
+  if (time <= 0 || time == null) {
+    alert(`The min video length should be 1s`);
+  } else {
+    if (time < 60) {
+      document.getElementById("dureation").innerHTML = `00:${time}`;
+    } else {
+      let cal_time = time / 60;
+      let min = Math.floor(cal_time);
+      let sec = time % 60;
+      if (cal_time < 60) {
+        document.getElementById("dureation").innerHTML = `${min}:${sec}`;
+      } else {
+        hr = min / 60;
+        document.getElementById("dureation").innerHTML = `${hr}:${min}:${sec}`;
+      }
+    }
+  }
+}
+
+// mere demag mai ek idea aaya hai jo function data set kare ga eg: setTime() use eventlistioner laga ke await karwa do jabtak me genereate per click na kardu.
+
+// =======Get title for video======
+function getTitle() {
+  let title = document.getElementById("input_title").value;
+  localStorage.setItem("title", title);
+}
+// --------set title--------
+function setTitle() {
+  let title = localStorage.getItem("title");
+  document.getElementById("title").children[0].innerHTML = title;
+}
+// =========Get channel name=====
+function getChannel() {
+  let chaname = document.getElementById("input_channel").value;
+  localStorage.setItem("channel_name", chaname);
+}
+// --------Set channel name----
+function setChannel() {
+  let chaname = localStorage.getItem("channel_name");
+  document.getElementsByClassName("link")[0].innerHTML = chaname;
+}
+// ===== Get views on that video=======
+function getViews() {
+  let view = document.getElementById("input_views").value;
+  localStorage.setItem("view", view);
+}
+// ---------Set views on your videos-------
+function setViews() {
+  let view = localStorage.getItem("view")*1; // multiplying by 1 converts the string to integer; I can use parseInt() too for this.
+  let show_view;
+  if (view >= 1000000) {
+    show_view = Math.floor(view / 1000000) + "M";
+  } else if (view >= 1000) {
+    show_view = Math.floor(view / 1000) + "K";
+  } else {
+    show_view = view;
+  }
+  document.getElementById("v").innerHTML = show_view;
+}
+// ========= Get how much old is video=======
